4e2a58a4b1e3dc0b4ecd519bdeeabb4a3dad6565
Florenzo Bauer <florenzobauer@gmail.com>
2023-10-29 13:29:11 -0600

    This is an updated html

--- main.js
+++ main.js
@@ -11,46 +11,46 @@ message.addEventListener("click", choice);
 clearButton.addEventListener("click", clear);
 
 function getRandomAffirmation() {
-    var randomAIndex = getRandomIndex(affirmations);
-    return affirmations[randomAIndex];
-  }
+  var randomAIndex = getRandomIndex(affirmations);
+  return affirmations[randomAIndex];
+}
 function getRandomMantra() {
-    var randomMIndex = getRandomIndex(mantras);
-    return mantras[randomMIndex];
+  var randomMIndex = getRandomIndex(mantras);
+  return mantras[randomMIndex];
+}
+
+function choice() {
+  if (affButton.checked == true) {
+    var randomAffirmation = getRandomAffirmation();
+    disMessage.innerText = randomAffirmation;
+  } else if (mantrasButton.checked == true) {
+    var randomMantra = getRandomMantra();
+    disMessage.innerText = randomMantra;
+  } else {
+    window.alert("Please select an option");
+    return;
   }
 
-  function choice() {
-    if (affButton.checked == true) {
-      var randomAffirmation = getRandomAffirmation();
-      disMessage.innerText = randomAffirmation;
-    } else if (mantrasButton.checked == true) {
-      var randomMantra = getRandomMantra();
-      disMessage.innerText = randomMantra;
-    } else {
-      window.alert("Please select an option");
-      return;
-    }
-  
-    logo.style.visibility = 'hidden';
-    clearButton.style.visibility = 'visible';
+  logo.style.visibility = 'hidden';
+  clearButton.style.visibility = 'visible';
 }
 function clear(){
-    clearButton.style.visibility = 'hidden';
-    logo.style.visibility = 'visible';
-    disMessage.innerText = "";
-    affButton.checked = false;
-    mantrasButton.checked = false;
+  clearButton.style.visibility = 'hidden';
+  logo.style.visibility = 'visible';
+  disMessage.innerText = "";
+  affButton.checked = false;
+  mantrasButton.checked = false;
 }
 function randomAffirmation() {
-var randomAIndex = getRandomIndex(affirmations);
-return randomAIndex;
+  var randomAIndex = getRandomIndex(affirmations);
+  return randomAIndex;
 }
 
 function randomMantra() {
-var randomMIndex = getRandomIndex(mantras);
-return randomMIndex;
+  var randomMIndex = getRandomIndex(mantras);
+  return randomMIndex;
 }
 
 function getRandomIndex(array) {
     return Math.floor(Math.random() * array.length);
-  }
+}
